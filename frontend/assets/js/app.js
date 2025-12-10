219f73e109fbdfd02d7fc9341ee3b76131c8c571
tsiokani <fanitsiok@gmail.com>
2025-12-10 18:41:46 +0200

    fade categories

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -47,3 +47,23 @@ if (window.location.pathname.includes("courses.html")) {
     
     renderCourses(COURSES);
 }
+
+if(window.location.pathname.includes("index.html")) {
+    const fadeSection = document.querySelector(".fade-section");
+
+    if(fadeSection) {
+        const observer = new IntersectionObserver((entries,obs) => {
+            entries.forEach(entry=> {
+                if(entry.isIntersecting){
+                    entry.target.classList.add("is-visible");
+                }else{
+                    entry.target.classList.remove("is-visible");
+
+                }
+            })
+        }, {threshold:0.2});
+        
+        observer.observe(fadeSection);
+    }
+}
+
