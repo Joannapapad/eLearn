22489d4e17ab7310a13add6e54f34e2b7c2cb32b
Ioanna Papad <ioannapapad04@gmail.com>
2025-12-11 16:50:54 +0200

    oopsies

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -1,5 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+    
+    if(window.location.pathname.includes("index.html")) {
+        const fadeSection = document.querySelector(".fade-section");
+    
+        if(fadeSection) {
+            const observer = new IntersectionObserver((entries,obs) => {
+                entries.forEach(entry=> {
+                    if(entry.isIntersecting){
+                        entry.target.classList.add("is-visible");
+                    }else{
+                        entry.target.classList.remove("is-visible");
+    
+                    }
+                })
+            }, {threshold:0.2});
+            
+            observer.observe(fadeSection);
+    
+    
+    
+        }
+    }
     function getParameterByName(name) {
         const urlParams = new URLSearchParams(window.location.search);
         return urlParams.get(name);
